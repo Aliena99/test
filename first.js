106e8b4fdb6870a28932af71c384a58d796927f3
elena <elena.aliena99@rubyteam.org>
2023-06-01 19:38:46 +0300

    new example

--- first.js
+++ first.js
@@ -1,2 +1,13 @@
-console.log('Hello');
-console.log('World!');
+var names = [ "Frank", "Kyle", "Peter", "Susan" ];
+
+names.length;
+// 4
+
+names[0];
+// Frank
+
+names[1];
+// Kyle
+console.log(names[1]);
+console.log(names[0]);
+console.log(names.length);
